e36a04396e03667ada655544c765437e864a3d5d
rap0so <william.s.godoy53@gmail.com>
2020-07-25 09:48:22 -0300

    Add challenge

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const carro = {
-  ano: '2015',
-  modelo: 'onix',
-  versao: 'tsi',
-}
-console.log(carro.ano)
+// Ao clicar em qualquer um dos botoes no html
+// o objetivo é que o texto do h1 se torne o valor dentro do botão
+
+// 🌟 bonus: o texto do h1 se tornar: "O clicado foi: [numero do botao aqui]"
+
+const name = document.getElementById('name')
+const button = document.getElementById('button')
+
+// button.addEventListener('click', function clickButton() {
+//   name.value = ''
+// })
