22ec3943ff76b1484468676a4690ca6d128dfbf9
LGondin <lucasgondin@hotmail.com>
2020-07-26 18:57:42 -0300

    inintial functions has been created

---
+++ showCards.js
@@ -0,0 +1,14 @@
+function showCards(cards) {
+  //  function for show cards in HTML, function get the cards elementos from archive createCards.js
+  const wrapperElement = document.getElementById('wrapper')
+
+  // Finalizar as funcionalidades montando um loop para mostrar no innerHTML os itens que resultarem do forEach
+  cards.forEach(function showMeCard(currentCard) {
+    const card = document.createElement('div')
+    card.className = 'card-style'
+    card.innerHTML = currentCard
+    wrapperElement.appendChild(card)
+  })
+}
+
+export default showCards
