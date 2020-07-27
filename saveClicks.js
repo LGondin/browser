137433df8c6df52b398bf6da00cfc48b662fb667
rap0so <william.s.godoy53@gmail.com>
2020-07-26 21:02:51 -0300

    Save both clicks

--- saveClicks.js
+++ saveClicks.js
@@ -1,8 +1,24 @@
-const elementsCardStyle = document.querySelectorAll('.card-style') // Const que me criou um array com todos os itens que tem classe card-style
+function saveClicks(callback) {
+  const elementsCardStyle = document.querySelectorAll('.card-style') // Const que me criou um array com todos os itens que tem classe card-style
+  const clicks = {
+    firstClickedCard: null,
+  }
 
-// array anterior rodando com um loop e usando a function para criar um evento click a click que da um log de?
-elementsCardStyle.forEach(function listCardStyle(currentItem) {
-  currentItem.addEventListener('click', function saveClick(event) {
-    console.log(event.target.className)
+  // array anterior rodando com um loop e usando a function para criar um evento click a click que da um log de?
+  elementsCardStyle.forEach(function listCard(currentItem) {
+    currentItem.addEventListener('click', function clickListener(event) {
+      if (!clicks.firstClickedCard) {
+        clicks.firstClickedCard = event.target.className
+      } else {
+        const secondClickedCard = event.target.className
+
+        if (clicks.firstClickedCard === secondClickedCard) {
+          callback(true)
+        }
+        clicks.firstClickedCard = null
+      }
+    })
   })
-})
+}
+
+export default saveClicks
