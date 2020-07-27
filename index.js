4c1f84bd40095535ce8bf939918d32e938237b5f
rap0so <william.s.godoy53@gmail.com>
2020-07-26 21:03:06 -0300

    Call function when compare clicks

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // create cards - OK
 // randomize cards - OK
 // show cards - OK
-// save first click card
-// save second click card
-// compare clicked cards
+// save first click card - OK
+// save second click card - OK
+// compare clicked cards - OK
 // send response to screen
 // if equal remove cards
 //  save points
@@ -12,6 +12,11 @@
 // turn cards down
 import createCards from './createCards.js'
 import showCards from './showCards.js'
+import saveClicks from './saveClicks.js'
 
 const cards = createCards()
 showCards(cards)
+
+saveClicks(function test(result) {
+  console.log(result)
+})
