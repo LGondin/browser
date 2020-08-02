c5a5fac529aaa0ff8ca5548306ce4c172f387ad8
LGondin <lucasgondin@hotmail.com>
2020-08-02 16:34:15 -0300

    new list bugs

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// create cards - OK
-// randomize cards - OK
-// show cards - OK
-// save first click card - OK
-// save second click card - OK
-// compare clicked cards - OK
-// send response to screen - TODO
-// if equal remove cards - OK
-//  save score - OK
-// else - OK
-//  remove score - OK
-// if there are no more cards - OK
-//  finish - OK
-// turn cards down - OK
-
 import createCards from './createCards.js'
 import showCards from './showCards.js'
 import saveClicks from './saveClicks.js'
