9f4af36e911618fcf1c600308f5da64c3057a8f6
LGondin <lucasgondin@hotmail.com>
2020-08-02 15:34:09 -0300

    import functions, add h3 and style this h3 from center

--- index.js
+++ index.js
@@ -6,28 +6,29 @@
 // compare clicked cards - OK
 // send response to screen - TODO
 // if equal remove cards - OK
-//  save score
-// else
-//  remove score
-// if there are no more cards
-//  finish
+//  save score - OK
+// else - OK
+//  remove score - OK
+// if there are no more cards - OK
+//  finish - OK
 // turn cards down
 
 import createCards from './createCards.js'
 import showCards from './showCards.js'
 import saveClicks from './saveClicks.js'
 import removeCards from './removeCards.js'
+import setScore from './setScore.js'
+import showFinishMessage from './showFinishMessage.js'
 
 const cards = createCards()
 showCards(cards)
 
 // TODO: replace `test` name to something better
 saveClicks(function test(codeCard) {
+  setScore(codeCard)
+
   if (codeCard) {
-    // success
     removeCards(codeCard)
-  } else {
-    // failure
-    window.alert(':(')
   }
+  showFinishMessage()
 })
