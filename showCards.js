bcb9436f05492e63c8bdd5689e9319e7d3fbb8d4
rap0so <william.s.godoy53@gmail.com>
2020-08-02 15:47:27 -0300

    Add flip card effect

--- showCards.js
+++ showCards.js
@@ -4,12 +4,27 @@ function showCards(cards) {
 
   // Finalizar as funcionalidades montando um loop para mostrar no innerHTML os itens que resultarem do forEach
   cards.forEach(function showMeCard(currentCard) {
+    // create card
     const card = document.createElement('div')
-    card.className = 'card-style'
+    card.className = 'card-style flip-card'
     card.dataset.color = currentCard
-    card.style.backgroundColor = currentCard
-
     wrapperElement.appendChild(card)
+
+    // create innercard
+    const innerCard = document.createElement('div')
+    innerCard.className = 'flip-card-inner'
+    card.appendChild(innerCard)
+
+    // back and front card
+    const frontCard = document.createElement('div')
+    const backCard = document.createElement('div')
+
+    frontCard.className = 'flip-card-front'
+    backCard.className = 'flip-card-back'
+    backCard.style.backgroundColor = currentCard
+
+    innerCard.appendChild(frontCard)
+    innerCard.appendChild(backCard)
   })
 }
 
