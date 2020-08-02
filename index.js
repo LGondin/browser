f99454df616fdcec13e42b7485f5c0cf627f321c
Lucas Gondin <67075004+LGondin@users.noreply.github.com>
2020-08-02 16:56:48 -0300

    Fix bugs (#4)
    * new list bugs
    
    * fix double click bug
    
    * new folder src
    
    * remove files from master folder
    
    * Changing colors
    
    Co-authored-by: rap0so <william.s.godoy53@gmail.com>

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
-// create cards - OK
-// randomize cards - OK
-// show cards - OK
-// save first click card - OK
-// save second click card - OK
-// compare clicked cards - OK
-// send response to screen - TODO
-// if equal remove cards - OK
-//  save score - OK
-// else - OK
-//  remove score - OK
-// if there are no more cards - OK
-//  finish - OK
-// turn cards down - OK
-
-import createCards from './createCards.js'
-import showCards from './showCards.js'
-import saveClicks from './saveClicks.js'
-import removeCards from './removeCards.js'
-import setScore from './setScore.js'
-import showFinishMessage from './showFinishMessage.js'
-import turnCardsDown from './turnCardsDown.js'
+import createCards from './src/createCards.js'
+import showCards from './src/showCards.js'
+import saveClicks from './src/saveClicks.js'
+import removeCards from './src/removeCards.js'
+import setScore from './src/setScore.js'
+import showFinishMessage from './src/showFinishMessage.js'
+import turnCardsDown from './src/turnCardsDown.js'
 
 const cards = createCards()
 showCards(cards)
 
-// TODO: replace `test` name to something better
 saveClicks(function test(codeCard) {
   setScore(codeCard)
   setTimeout(function waitForSeeColor() {
---
+++ src/createCards.js
@@ -0,0 +1,33 @@
+function createCards() {
+  const cards = [
+    '#FF5733',
+    '#FFFC33',
+    '#61FF33',
+    '#33FFAC',
+    '#33D7FF',
+    '#339FFF',
+    '#4233FF',
+    '#9033FF',
+    '#C733FF',
+    '#FF33FC',
+    '#FF33BE',
+    '#FF3374',
+    '#6C6C6C',
+    '#DADADA',
+    '#269500',
+    '#955100',
+    '#C70039',
+    '#DAF7A6',
+    '#000000',
+  ]
+
+  const doubleCards = [...cards, ...cards]
+
+  const randomCards = doubleCards.sort(function shuffleCard() {
+    return 0.5 - Math.random()
+  })
+
+  return randomCards
+}
+
+export default createCards
