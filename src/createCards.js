784a289c1d89c4d6ce5f1c6c813ede2065ec4a69
rap0so <william.s.godoy53@gmail.com>
2020-08-02 16:52:24 -0300

    Changing colors

--- src/createCards.js
+++ src/createCards.js
@@ -1,16 +1,26 @@
 function createCards() {
   const cards = [
-    '#00FFFF',
-    '#00CED1',
-    '#40E0D0',
-    '#48D1CC',
-    '#20B2AA',
-    '#008B8B',
-    '#008080',
-    '#7FFFD4',
-    '#66CDAA',
-    '#5F9EA0',
+    '#FF5733',
+    '#FFFC33',
+    '#61FF33',
+    '#33FFAC',
+    '#33D7FF',
+    '#339FFF',
+    '#4233FF',
+    '#9033FF',
+    '#C733FF',
+    '#FF33FC',
+    '#FF33BE',
+    '#FF3374',
+    '#6C6C6C',
+    '#DADADA',
+    '#269500',
+    '#955100',
+    '#C70039',
+    '#DAF7A6',
+    '#000000',
   ]
+
   const doubleCards = [...cards, ...cards]
 
   const randomCards = doubleCards.sort(function shuffleCard() {
